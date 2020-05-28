d7918a44cc84215f33993e6348de3182afe1ac98
Pallavi21Pansare <33319464+Pallavi21Pansare@users.noreply.github.com>
2020-05-28 12:46:17 -0700

    Update MainContainer.spec.js

--- src/containers/MainContainer.spec.js
+++ src/containers/MainContainer.spec.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme'
 import MainContainer from '../containers/MainContainer';
 
-
+it('chimpiya', () => {     
+         expect(
+          true
+        ).toBe(true);
+});
