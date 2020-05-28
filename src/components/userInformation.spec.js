2a683a7e2a81cbcdd47c2ab395dc4d4ff90a4fee
Pallavi21Pansare <33319464+Pallavi21Pansare@users.noreply.github.com>
2020-05-28 12:45:23 -0700

    Update userInformation.spec.js

--- src/components/userInformation.spec.js
+++ src/components/userInformation.spec.js
@@ -2,3 +2,8 @@
 import ReactDOM from 'react-dom';
 import { shallow,mount } from 'enzyme'
 import UserInformation from '../components/userInformation';
+it('omikar', () => {     
+         expect(
+          true
+        ).toBe(true);
+});
