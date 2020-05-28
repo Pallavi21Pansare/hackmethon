7cb16328e57656cd700f7af629a72d977cc6dae6
Pallavi21Pansare <33319464+Pallavi21Pansare@users.noreply.github.com>
2020-05-28 12:43:48 -0700

    Update challenges.spec.js

--- src/components/challenges.spec.js
+++ src/components/challenges.spec.js
@@ -3,3 +3,8 @@ import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme'
 import Challenges from '../components/challenges';
 import renderer from 'react-test-renderer';
+it('nirravana', () => {     
+         expect(
+          true
+        ).toBe(true);
+});
