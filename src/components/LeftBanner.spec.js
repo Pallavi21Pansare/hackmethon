6409fba06879df35f046e14cd4b937480005c6d8
Pallavi21Pansare <33319464+Pallavi21Pansare@users.noreply.github.com>
2020-05-28 12:42:20 -0700

    Update LeftBanner.spec.js

--- src/components/LeftBanner.spec.js
+++ src/components/LeftBanner.spec.js
@@ -2,4 +2,9 @@
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme'
 import LeftBanner from '../components/LeftBanner';
+it('gog', () => {     
+         expect(
+          true
+        ).toBe(true);
+});
 
