50ae8f3f0b2aede5e9e7c4df4a884a3320d110ac
Pallavi21Pansare <33319464+Pallavi21Pansare@users.noreply.github.com>
2020-05-28 12:41:42 -0700

    Update Finish.spec.js

--- src/components/Finish.spec.js
+++ src/components/Finish.spec.js
@@ -2,5 +2,10 @@
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme'
 import Finish from '../components/Finish';
+it('avengers', () => {     
+         expect(
+          true
+        ).toBe(true);
+});
 
 
