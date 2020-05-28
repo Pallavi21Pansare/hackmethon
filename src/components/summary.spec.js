f4cacd007b271dc98c94813116a61e9ebfc52a96
Pallavi21Pansare <33319464+Pallavi21Pansare@users.noreply.github.com>
2020-05-28 12:44:23 -0700

    Update summary.spec.js

--- src/components/summary.spec.js
+++ src/components/summary.spec.js
@@ -2,3 +2,8 @@
 import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme'
 import Review from '../components/summary';
+it('anuja', () => {     
+         expect(
+          true
+        ).toBe(true);
+});
